a250ab15ec2e33c1c3e380b06df67c644c046e76
mengliang.yang <mengliang.yang555@gmail.com>
2022-05-13 23:14:13 +0800

    go generics update

--- go_try/go_generics.go
+++ go_try/go_generics.go
@@ -1,19 +1,55 @@
 package go_try
 
-func GetMapValue[K comparable, V int64 | float64](m map[K]V) V {
-	var s V
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
 type Number interface {
 	~int | ~float64 | ~float32 | ~int8 | ~int32 | ~int64
 }
 
-func AddValue[K Number](k1, k2 K) K {
-	return k1 + k2
+func GetValIndex[V any](val []V, target V, compare func(v1, v2 V) bool) int {
+	for i, v := range val {
+		if compare(v, target) {
+			return i
+		}
+	}
+	return -1
+}
+
+// SortSince todo 实现相关的排序算法
+func SortSince[V any](val []V, compare func(v1, v2 V) int) {
+	if len(val) < 2 {
+		return
+	}
+	for i := 0; i < len(val)-1; i++ {
+		for j := i + 1; j < len(val); j++ {
+			if compare(val[i], val[j]) > 0 {
+				val[i], val[j] = val[j], val[i]
+			}
+		}
+	}
+}
+
+func GetMapFromSlice[Val any, K comparable](val []Val, getKey func(val Val) K) map[K]Val {
+	ans := make(map[K]Val)
+	for _, v := range val {
+		ans[getKey(v)] = v
+	}
+	return ans
+}
+
+func DoIt[V any](param []V, behave func(s V)) {
+	for i, v := range param {
+		behave(v)
+		if i == len(param)-1 {
+			println()
+		}
+	}
+}
+
+func GetKeyFromMap[Val any, K comparable](valMap map[K]Val) []K {
+	ans := make([]K, 0, len(valMap))
+	for k := range valMap {
+		ans = append(ans, k)
+	}
+	return ans
 }
 
 func Max[K Number](k1, k2 K) K {
--- go_try/go_generics_test.go
+++ go_try/go_generics_test.go
@@ -1,6 +1,7 @@
 package go_try
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -24,4 +25,75 @@ func FuzzMaxAndFloat(f *testing.F) {
 			t.Error("code error")
 		}
 	})
+}
+
+func FuzzMaxAndInt1(f *testing.F) {
+	f.Add(10, 2)
+	f.Fuzz(func(t *testing.T, k1, k2 int) {
+		max := Max[int](k1, k2)
+		min := Min[int](k1, k2)
+		if max < min {
+			t.Error("code error")
+		}
+	})
+}
+
+func FuzzMaxAndFloat1(f *testing.F) {
+	f.Add(float32(3.0), float32(2.1))
+	f.Fuzz(func(t *testing.T, k1, k2 float32) {
+		max := Max[float32](k1, k2)
+		min := Min[float32](k1, k2)
+		if max < min {
+			t.Error("code error")
+		}
+	})
+}
+
+type testStruct struct {
+	name string
+	age  int32
+}
+
+func TestGetMapFromSlice(t *testing.T) {
+	val := []testStruct{{"hello", 12}, {"world", 13}}
+	ans := GetMapFromSlice[testStruct, string](val, func(v testStruct) string { return v.name })
+	for key, val := range ans {
+		fmt.Printf("key:%s val:%v", key, val)
+	}
+}
+
+func TestGetKeyFromMap(t *testing.T) {
+	target := []int{1, 2, 3, 4, 5, 6}
+	ans := GetMapFromSlice[int, int](target, func(val int) int { return val * val })
+	fi := GetKeyFromMap[int, int](ans)
+	fmt.Println(fi)
+}
+
+func TestDoIt(t *testing.T) {
+	target := []int{1, 2, 3, 4, 5, 6}
+	DoIt[int](target, func(v int) { println(v) })
+}
+
+func TestGetValIndex(t *testing.T) {
+	target := []int{1 + 1, 2 + 4, 3 + 6, 4 + 8, 5 + 12, 6 + 15}
+	print(GetValIndex[int](target, 6, func(v1, v2 int) bool { return v1 == v2 }))
+}
+
+func TestGetValIndex1(t *testing.T) {
+	target := []int{1 + 1, 2 + 4, 3 + 6, 4 + 8, 5 + 12, 6 + 15}
+	print(GetValIndex[int](target, 100, func(v1, v2 int) bool { return v1 == v2 }))
+}
+
+func TestSort(t *testing.T) {
+	target := []int{1 + 11, 2 + 1, 3 + 2, 4 + 8, 5 + 12, 6 + 2}
+	DoIt[int](target, func(v int) { fmt.Printf("%d\t", v) })
+	SortSince[int](target, func(v1, v2 int) int { return v1 - v2 })
+	DoIt[int](target, func(v int) { fmt.Printf("%d\t", v) })
+}
+
+func TestSort2(t *testing.T) {
+	target := []int{1 + 11}
+	DoIt[int](target, func(v int) { fmt.Printf("%d\t", v) })
+	SortSince[int](target, func(v1, v2 int) int { return v1 - v2 })
+	DoIt[int](target, func(v int) { fmt.Printf("%d\t", v) })
 }
